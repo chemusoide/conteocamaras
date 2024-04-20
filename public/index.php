3286e2d2259e38b6f95e958390647e48e55beb23
chemusoide <chema@informaticapolo.com>
2024-04-20 13:09:46 +0200

    programa funciona con las dos vistas

--- public/index.php
+++ public/index.php
@@ -2,26 +2,29 @@
 // Establecer la zona horaria a España (UTC+2)
 date_default_timezone_set('Europe/Madrid');
 
+$view = $_GET['view'] ?? 'default'; // 'default' es la vista por defecto
+
 // Obtener la ruta al archivo de configuración de la cámara
 $configFilePath = __DIR__ . '/../config/cameraInfo.json';
 
-// Verificar si el archivo de configuración existe
 if (file_exists($configFilePath)) {
-    // Cargar el controlador correspondiente según la solicitud
     require_once __DIR__ . '/../src/controllers/CameraController.php';
-
-    // Crear una instancia del controlador
     $controller = new CameraController();
-
-    // Calcular el aforo total directamente desde el controlador, sin necesidad de pasar nombres de cámara
     $aforoData = $controller->calcularAforoTotal();
-
-    // Extraer los datos para facilitar el acceso en la vista
     extract($aforoData);
 
-    // Ahora incluye la vista con la ruta corregida
-    require_once __DIR__ . '/../src/views/aforo_total_view.php';
+    // Decide qué vista cargar en función del parámetro 'view'
+    switch ($view) {
+        case 'simple':
+            require_once __DIR__ . '/../src/views/aforo_total_simple.php';
+            break;
+        case 'default':
+            require_once __DIR__ . '/../src/views/aforo_total_view.php';
+            break;
+        default:
+            echo "Error: Vista no reconocida.";
+            break;
+    }
 } else {
-    // Si el archivo de configuración no se encuentra, mostrar un mensaje de error
     echo "Error: No se encontró el archivo de configuración cameraInfo.json.";
 }
